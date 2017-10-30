f2d2b959b6371126947e6e88908514196bc582a3
Nash <18695616095@163.com>
2017-10-30 23:58:54 +0800

    主页上传

--- src/App/Main/Store.vue
+++ src/App/Main/Store.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+    <Row class="block">
+      <Col span="24" class="title">
+        主编推荐<a href="#">查看更多 <Icon type="ios-arrow-forward"></Icon></a>
+      </Col>
+      <Col span="24" class="body">
+        <Row :gutter="12" id="store-head-book">
+          <Col span="8">
+            <img src="http://m.i890.net/files/article/image/2/2728/2728s.jpg">
+            <div>厚爱成婚</div>
+          </Col>
+          <Col span="8">
+            <img src="http://m.i890.net/files/article/image/2/2728/2728s.jpg">
+            <div>厚爱成婚</div>
+          </Col>
+          <Col span="8">
+            <img src="http://m.i890.net/files/article/image/2/2728/2728s.jpg">
+            <div>厚爱成婚</div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   </div>
 </template>
 
