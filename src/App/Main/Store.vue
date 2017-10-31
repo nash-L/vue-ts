4a971b2ffa5cc7c5cace3a3f0b9b6ee9c3854ea9
Nash <18695616095@163.com>
2017-10-31 18:04:28 +0800

    主页设计上传

--- src/App/Main/Store.vue
+++ src/App/Main/Store.vue
@@ -5,7 +5,7 @@
         主编推荐<a href="#">查看更多 <Icon type="ios-arrow-forward"></Icon></a>
       </Col>
       <Col span="24" class="body">
-        <Row :gutter="12" id="store-head-book">
+        <Row :gutter="12" class="store-head-book">
           <Col span="8">
             <img src="http://m.i890.net/files/article/image/2/2728/2728s.jpg">
             <div>厚爱成婚</div>
@@ -19,6 +19,47 @@
             <div>厚爱成婚</div>
           </Col>
         </Row>
+      </Col>
+    </Row>
+    <Row class="block">
+      <Col span="24" class="title">
+        书城精选
+      </Col>
+      <Col span="24" class="body">
+        <Row :gutter="12" class="store-main-book">
+          <Col span="6">
+            <img src="http://m.hongchenbook.com/files/article/image/2/2176/2176s.jpg">
+          </Col>
+          <Col span="18">
+            <h4>女神情衷</h4>
+            <small>挣脱婚枷异乡职场去竞争 誓纾母忧到底逆袭上巅峰 冥冥之中疑有伟人牵丝红 茫茫人海旷世奇缘演绎中...</small>
+          </Col>
+        </Row>
+        <Row :gutter="12" class="store-main-book">
+          <Col span="6">
+            <img src="http://m.hongchenbook.com/files/article/image/2/2176/2176s.jpg">
+          </Col>
+          <Col span="18">
+            <h4>女神情衷</h4>
+            <small>挣脱婚枷异乡职场去竞争 誓纾母忧到底逆袭上巅峰 冥冥之中疑有伟人牵丝红 茫茫人海旷世奇缘演绎中...</small>
+          </Col>
+        </Row>
+        <Row :gutter="12" class="store-main-book">
+          <Col span="6">
+            <img src="http://m.hongchenbook.com/files/article/image/2/2176/2176s.jpg">
+          </Col>
+          <Col span="18">
+            <h4>女神情衷</h4>
+            <small>挣脱婚枷异乡职场去竞争 誓纾母忧到底逆袭上巅峰 冥冥之中疑有伟人牵丝红 茫茫人海旷世奇缘演绎中...</small>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+    <Row class="block">
+      <Col span="24" class="title">
+        热门阅读
+      </Col>
+      <Col span="24" class="body">
       </Col>
     </Row>
   </div>
